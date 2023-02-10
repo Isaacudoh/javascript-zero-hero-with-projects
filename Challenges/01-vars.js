e7e3f6a587b1061fd07167630cd1e088be2e7d36
nsikakudoh <nsikak@sparknerds.io>
2023-02-10 15:44:16 -0700

    complete first challenge

---
+++ Challenges/01-vars.js
@@ -0,0 +1,16 @@
+/* Variables #1
+
+1. create "firstname" and "last_name" variables
+2. assign your values
+3. create "address variable" and assign "main street" value to it
+4. re-assign address to "first street" later
+5. log all vlaues in the console 
+
+*/
+
+let firstName = "nsikak";
+let last_name = "udoh";
+let address = "main street";
+address = "first street";
+
+console.log(firstName, last_name, address);
--- Challenges/README.md
+++ Challenges/README.md
@@ -1,2 +1,7 @@
 ## Variables #1
 
+1. create "firstname" and "last name" variables
+2. assign your values
+3. create "address variable" and assign "main street" value to it
+4. re-assign address to "first street" later
+5. log all vlaues in the console
