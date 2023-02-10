81d47c71469725099388072ef071a53f9cee3601
nsikakudoh <nsikak@sparknerds.io>
2023-02-10 16:23:46 -0700

    string concatenation

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-// "" or ''
+// string concatenation - combine string values
+// `` - backticks (template strings) easier option.
 
-let name = "nsikak's food is the best";
-console.log(name);
+// let name = "nsikak";
+// let lastName = "udoh";
+// let fullName = name + " " + lastName;
+
+// console.log("hello there my full name is : " + fullName);
+
+let website = "google";
+let url = "https://www." + website + ".com"
+
+console.log(url);
+
