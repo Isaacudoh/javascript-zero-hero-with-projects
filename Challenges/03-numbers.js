a55ce74a024e6fd43f286bfa1a7719b80ee0018f
nsikakudoh <nsikak@sparknerds.io>
2023-02-11 09:34:29 -0700

    complete challenge 3

--- Challenges/03-numbers.js
+++ Challenges/03-numbers.js
@@ -13,5 +13,22 @@ let score3 = 12;
 let totalScore = score1 + score2 + score3;
 let averageScore = totalScore / 3;
 
-console.log(totalScore);
-console.log(averageScore);
+// console.log(totalScore);
+// console.log(averageScore);
+
+/*
+4. create "plates" variable and assign 20
+5. create "people" variable and assign 7 
+6. calculate remaining plates and assign to the variable
+7. add one to remaining plates
+8. create message varible and display 'There are (your value goes here) plates available' - string concatenation
+9. Log message
+*/
+
+let plates = 20;
+let people = 7;
+let remainingPlates = plates % people;
+// console.log(remainingPlates);
+remainingPlates++;
+let message = "There are" + " " + remainingPlates + " " + "plates available";
+console.log(message);
