ffb1d1f4809151a5a8bc1c3481aeed31da383ae2
nsikakudoh <nsikak@sparknerds.io>
2023-02-11 11:27:35 -0700

    complete arrays challenge

--- Challenges/04-arrays.js
+++ Challenges/04-arrays.js
@@ -3,9 +3,15 @@
 ## Arrays #4
 
 1. create "fruits" array and store some fruits values
-2. setup the last item s a number (random)
+2. setup the last item as a number (random)
 3. assign first fruit to the variable
 4. re-assign last array item to the actual fruit
 5. log both first fruit variable and entire fruits array
 
 */
+
+let fruits = ["apple", "bannana", "grapes", "starapple", 54];
+let firstFruit = fruits[0];
+console.log(firstFruit);
+fruits[4] = 'pear';
+console.log(fruits)
