df19cc0344d22ad566cd6bd51aa621ea1a221531
nsikakudoh <nsikak@sparknerds.io>
2023-02-11 08:36:02 -0700

    complete challenge #2 concatenate strings

---
+++ Challenges/02-concat.js
@@ -0,0 +1,12 @@
+// ## Concatenate Stings #2
+// 1. create "street" and "country" variables
+// 2. assign them value
+// 3. create "fullMailingAddress" variable and assign the result of "street + country"
+// 4. remember the space
+// 5. log "fullMailingAddress" in the console
+
+let street = "814 N magnolia st";
+let country = "USA";
+let fullMailingAddress = street + " " + country;
+
+console.log(fullMailingAddress);
--- Challenges/README.md
+++ Challenges/README.md
@@ -5,3 +5,10 @@
 3. create "address variable" and assign "main street" value to it
 4. re-assign address to "first street" later
 5. log all vlaues in the console
+
+## Concatenate Stings #2
+1. create "street" and "country" variables
+2. assign them value
+3. create "fullMailingAddress" variable and assign the result of "street + country"
+4. remember the space
+5. log "fullMailingAddress" in the console
