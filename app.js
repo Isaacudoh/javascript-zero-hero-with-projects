fbe2dfba6c26f16568b3e53ba0d0eeb9616b30ea
nsikakudoh <nsikak@sparknerds.io>
2023-02-11 11:09:16 -0700

    complete arrays

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 // Arrays, Functions and Objects
 // Arrays - [], 0 index based
+
+//Arrays give us an Easy way to store, access, perform operations from list of items with ease
+
+let friend1 = "anna";
+let friend2 = "anna";
+let friend3 = "anna";
+let friend4 = "anna";
+let friend5 = "anna";
+
+let friends = ["jack", "john", "Mary", "bob", 45, undefined, null];
+console.log(friends[12]);
