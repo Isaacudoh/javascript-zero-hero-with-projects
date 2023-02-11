f10da64e0c64bdd14d6806f14ebefa203d84fb5d
nsikakudoh <nsikak@sparknerds.io>
2023-02-11 09:23:09 -0700

    complete first 3 challenge

---
+++ Challenges/03-numbers.js
@@ -0,0 +1,17 @@
+/*
+
+Number #3
+1. create "score1", "score2", "score3", variables and sssign them values (0-100)
+2. calculate total score and average score, and assign them to the variables.
+3. log total score and average score
+*/
+
+let score1 = 8;
+let score2 = 4;
+let score3 = 12;
+
+let totalScore = score1 + score2 + score3;
+let averageScore = totalScore / 3;
+
+console.log(totalScore);
+console.log(averageScore);
