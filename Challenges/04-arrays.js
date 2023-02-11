d61b59d0c9190b7ad39fa1f8272c3e9ae159d702
nsikakudoh <nsikak@sparknerds.io>
2023-02-11 11:12:39 -0700

    start arrays challenge

---
+++ Challenges/04-arrays.js
@@ -0,0 +1,11 @@
+/*
+
+## Arrays #4
+
+1. create "fruits" array and store some fruits values
+2. setup the last item s a number (random)
+3. assign first fruit to the variable
+4. re-assign last array item to the actual fruit
+5. log both first fruit variable and entire fruits array
+
+*/
