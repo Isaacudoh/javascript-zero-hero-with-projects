20b5934b3abdbe83a23efcc62a6a3644186552b0
nsikakudoh <nsikak@sparknerds.io>
2023-02-11 08:50:07 -0700

    numbers data type

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-// string concatenation - combine string values
-// `` - backticks (template strings) easier option.
+// numbers
+// loosely typed = don;t declare type
 
-// let name = "nsikak";
-// let lastName = "udoh";
-// let fullName = name + " " + lastName;
 
-// console.log("hello there my full name is : " + fullName);
-
-let website = "google";
-let url = "https://www." + website + ".com"
-
-console.log(url);
 
