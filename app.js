559564ba50c260ad2628f723584453692e93a77e
nsikakudoh <nsikak@sparknerds.io>
2023-02-11 09:07:27 -0700

    complete modular

--- app.js
+++ app.js
@@ -3,6 +3,11 @@
 // +=, -=, ++ , --, %
 // Modulus (%) operator returns the remainder after integer division
 
+let number = 5;
+number += 5;
+console.log(number);
 
-
-
+let pizzaSlices = 10;
+let children = 3;
+let amount = pizzaSlices % children;
+console.log(amount)
