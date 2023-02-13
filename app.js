4ec0787803ec22c23bfa1ededcb2f4d54acdfd57
nsikakudoh <nsikak@sparknerds.io>
2023-02-13 10:58:48 -0700

    first function

--- app.js
+++ app.js
@@ -3,4 +3,11 @@
 
 // functions allows us write our code in one place and later reuse that code all throughout our application without copy paste.
 
+function hello() {
+  //   logic
+  console.log("hello there bob");
+  console.log("hello there tom");
+  console.log("hello there abi");
+}
 
+hello();
