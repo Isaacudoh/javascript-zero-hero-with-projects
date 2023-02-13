73b6a6a712333992be56da2619cd48c1f4633cf3
nsikakudoh <nsikak@sparknerds.io>
2023-02-13 12:13:40 -0700

    start basic return

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
 // Arrays, Functions and Objects
-// params - when declare/invote - Parameter are like placeholder or local variable that we can pass value to and when we pass that value its called an argument.
-// placeholders, local vars
-// arguments - when invoke/call/run
-// use vars/values, multiple params, undefined
-
-function greet(name) {
-  console.log("Hello there" + " " + name);
-}
-
-// greet nsikak
-greet("nsikak");
-
-// greet bob
-greet("bob");
-
-// greet anna
-greet("anna")
-
-// greet susy
+// return
+// defualt undefined, shortcuts, ignores after
