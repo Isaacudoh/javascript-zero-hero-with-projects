7a7c84d125e8c2fe07f32f82df0155951a33b899
nsikakudoh <nsikak@sparknerds.io>
2023-02-13 12:45:44 -0700

    complete return basic

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 // Arrays, Functions and Objects
 // return
 // defualt undefined, shortcuts, ignores after
+//  1 inch 2.5cm - write a function that calculates value in cm
+
+function calculate (value){
+    console.log ("The value in cm is: " + value * 2.54 + "cm")
+}
+
+calculate(24);
+
+// let width = 0;
+// let height = 0;
+
+// let dimensions = [width, length];
+
+// console.log(dimensions);
