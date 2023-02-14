dd4d43c9d07478fcbdc5d4ed61e499769a694ce0
nsikakudoh <nsikak@sparknerds.io>
2023-02-14 09:46:56 -0700

    start objects challenge

---
+++ Challenges/06-Objects.js
@@ -0,0 +1,14 @@
+/*
+
+## objects #6
+
+1. create car object
+2. add make, model, year, colors (array), hydrid (boolean) keys
+3. add two methods (drive and stop)
+4. in the function body setup log with random text
+5. log make
+6. log first color
+7. invoke both methods
+
+*/
+
