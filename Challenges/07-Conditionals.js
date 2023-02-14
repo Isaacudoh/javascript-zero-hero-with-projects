99a1d4ee24ff4c418a264b298b0a41727635d16b
nsikakudoh <nsikak@sparknerds.io>
2023-02-14 13:52:52 -0700

    revised solution conditional challenge

--- Challenges/07-Conditionals.js
+++ Challenges/07-Conditionals.js
@@ -11,27 +11,24 @@
 
 let person1 = {
   name: "nsikak",
-  age: 15,
-  status: 'resident',
+  age: 56,
+  status: "resident",
 };
 
 let person2 = {
-    name: 'john',
-    age: 25,
-    status: 'tourist'
-}
+  name: "john",
+  age: 15,
+  status: "tourist",
+};
 
-if(person1.age >= 18 && person1.status === resident){
-    console.log("You are over 18 and is a resident")
-}
-else {
-    console.log('dont be a brat')
-}
-
-if (person2.age > 18 && status === person2.resident) {
-  console.log("You are over 18 and is a resident");
+if (person1.age >= 18 && person1.status === "resident") {
+  console.log("You can vote because you are over 18 & is a resident");
 } else {
   console.log("dont be a brat");
 }
 
-
+if (person2.age < 18 || person2.status !== "resident") {
+  console.log("You are NOT over 18 or a resident");
+} else {
+  console.log("You go vote now");
+}
