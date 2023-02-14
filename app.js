20ddfe3079723cc3a82c6d5c40d5890cceab2ced
nsikakudoh <nsikak@sparknerds.io>
2023-02-14 12:48:02 -0700

    complete all if statement

--- app.js
+++ app.js
@@ -1,4 +1,42 @@
 // Switch
 // dice value : 1 - 6
 
+let dice = 1;
 
+// else if
+if (dice === 1) {
+  console.log("You got one");
+}
+if (dice === 2) {
+  console.log("You got two");
+}
+if (dice < 1 || dice > 6) {
+  console.log("error: You did not roll the dice");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// All if
+
+// if(dice === 1){
+//   console.log("You got one")
+// }
+// if(dice === 2){
+//   console.log ("You got two")
+// }
+// if(dice < 1 || dice > 6 ){
+//   console.log('error: You did not roll the dice')
+// }
