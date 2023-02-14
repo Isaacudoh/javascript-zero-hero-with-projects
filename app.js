616fbaabecdb1cdc7dd77501c28e686fa49f67e6
nsikakudoh <nsikak@sparknerds.io>
2023-02-14 09:24:54 -0700

    start objects

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
 // Arrays, Functions and Objects
-// Expressions - another way to define a function
-// creat a varibale, assign to function (not vlaue), use var
-//  diff - hositing, use - arrow func, libraries
-
-// function defination
-function addValues(num1, num2) {
-  return num1 + num2;
-}
-
-let firstValue = addValues(3, 4);
-let secondValue = addValues(12, 34);
-
-// function expression
-let values = [firstValue, secondValue];
-console.log(values);
+// Objects - Key/Value pairs method
+// dot notation
