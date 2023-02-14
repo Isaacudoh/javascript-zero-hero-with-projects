c6a0ab73458888777d6a315d22badcb0d702390a
nsikakudoh <nsikak@sparknerds.io>
2023-02-14 12:29:54 -0700

    start logical operator

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-// conditional statements
-// comparison operators
-// >, <, >=, <=, ==, ===, !=, !===
-// ==check only value
-// === checks value and type
+// logical operator
+// (|| - OR), (&& - AND), !
 
-let num1 = 6;
-let num2 = 6;
 
-let value = num1 == num2;
-let value2 = num1 === num2;
 
-console.log(value);
-console.log(value2);
