d9d58ed7914b9850ba23df962ca66ba1e405e095
nsikakudoh <nsikak@sparknerds.io>
2023-02-14 12:36:38 -0700

    complete logical operators

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // logical operator
 // (|| - OR), (&& - AND), !
 
+let name = "nsikak";
+let age = 32;
 
-
+if (name === "nsikak" && age === 31) {
+  console.log("hello user");
+} else {
+  console.log("wrong value");
+}
