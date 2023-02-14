1bbee3271c49bc595e9926c070bebd404c435320
nsikakudoh <nsikak@sparknerds.io>
2023-02-14 13:02:34 -0700

    complete switch statement

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
 // Switch
 // dice value : 1 - 6
 
-let dice = 0;
+let dice = 3;
+
+switch (dice) {
+  case 1:
+    console.log("you got one");
+    break;
+  case 2:
+    console.log("you got two");
+    break;
+  case 3:
+    console.log("you got three");
+    break;
+    default:
+      console.log('you did not roll the dice')
+}
 
 // else if
-if (dice === 1) {
-  console.log("You got one");
-}
-else if (dice === 2) {
-  console.log("You got two");
-}
-else {
-  console.log("error: You did not roll the dice");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// if (dice === 1) {
+//   console.log("You got one");
+// }
+// else if (dice === 2) {
+//   console.log("You got two");
+// }
+// else {
+//   console.log("error: You did not roll the dice");
+// }
 
 // All if
 
