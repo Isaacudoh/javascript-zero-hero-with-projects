899d3719ab8679ad84516d4c925e912ef08e59d5
nsikakudoh <nsikak@sparknerds.io>
2023-02-15 10:17:04 -0700

    decrement loop

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // loops
-// repeatedly run a block of code while confition is true
+// repeatedly run a block of code while condition is true
 // for loops
 
-let i;
-for (i = 0; i < 10; i++) {
-  console.log("and the number is : " + i);
+// let i;
+// for (i = 0; i < 10; i++) {
+//   console.log("and the number is : " + i);
+// }
+
+for (let number = 11; number >= 0; number--) {
+  console.log("and the number is : " + number);
 }
