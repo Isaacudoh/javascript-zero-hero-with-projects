9eeee9e18c2ef89422fad8fc8f17daf414c5efb0
nsikakudoh <nsikak@sparknerds.io>
2023-02-15 14:20:56 -0700

    array splice method

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 // Array Properties and Methods
 
-let name = ["john", "bobo", "barry", "olga", "ben"];
+let names = ["john", "bobo", "barry", "olga", "ben"];
+
+// length
+console.log(names.length);
+console.log(names[names.length - 1]);
+
+// concat
+let lastNames = ["pepper", "onion", "banana"];
+let allNames = names.concat(lastNames);
+console.log(allNames);
+
+// reserve
+console.log(allNames.reverse());
+
+// unshift - adds to beginning of array
+allNames.unshift("susy");
+console.log(allNames);
+
+// shift - remove item from begining of array
+allNames.shift();
+console.log(allNames);
+
+// push - add to end of array
+allNames.push("susy");
+console.log(allNames);
+
+// pop -
+allNames.pop();
+console.log(allNames);
