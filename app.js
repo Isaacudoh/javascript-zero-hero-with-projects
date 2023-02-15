30e7abbb26570dc41555027c83db5bdf9dec4b6b
nsikakudoh <nsikak@sparknerds.io>
2023-02-15 12:38:50 -0700

    array properties and methods

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-// Template literals - ES6+
-// Backtick characters `` - about tab (left from one)
-// Interpolation ${} - insert expression(value)
+// Array Properties and Methods
 
-let name = "john";
-let age = 25;
-let sentence = "Hey it's " + name + " and he is " + age + "";
-
-let value = `Hey it's ${name} and he is ${age} years old. and here is some simple math ${
-  4 * 5
-}`;
-
-// console.log(sentence);
-
-console.log(value);
+let name = ["john", "bobo", "barry", "olga", "ben"];
