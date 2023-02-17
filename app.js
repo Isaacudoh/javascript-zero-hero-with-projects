4df396c1c2fbe016f09c761b902efba32ac9d718
nsikakudoh <nsikak@sparknerds.io>
2023-02-17 15:41:26 -0700

    template literals and lastName

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let newArray = [];
 for (let i = 0; i < names.length; i++) {
   console.log(i);
   console.log(names[i]);
-  newArray.push(names[i]);
+  newArray.push(`${names[i]} ${lastName}`);
 }
 
 console.log(names);
