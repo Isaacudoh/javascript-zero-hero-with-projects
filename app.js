87215be82d16432029f9adcae7a22fdf1d1c6669
nsikakudoh <nsikak@sparknerds.io>
2023-02-17 15:39:14 -0700

    use push method

--- app.js
+++ app.js
@@ -9,4 +9,8 @@ let newArray = [];
 for (let i = 0; i < names.length; i++) {
   console.log(i);
   console.log(names[i]);
+  newArray.push(names[i]);
 }
+
+console.log(names);
+console.log(newArray);
