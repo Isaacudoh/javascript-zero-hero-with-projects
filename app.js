4dba239ab51d24413d44b523ea437a876bcbb5f8
nsikakudoh <nsikak@sparknerds.io>
2023-02-23 13:49:54 -0700

    complete self challenge

--- app.js
+++ app.js
@@ -1,15 +1,66 @@
-// Functions, return, if, arrays, for loop
+// Further self practice
 
-let gas = [20, 40, 100, 30];
-let food = [10, 40, 50];
+// #1 — Print the numbers from 0 to 15
 
-function calculateTotal(arr) {
-  let total = 0;
-  for (let i = 0; i < arr.length; i++) {
-    total += arr[i];
-  }
-  return total;
+for (let i = 0; i <= 15; i++) {
+  // console.log("the value of i is:" + i);
 }
 
-let gasTotal = calculateTotal(gas);
-let foodTotal = calculateTotal(food);
+// #2 — Print the numbers from 12 to 24
+
+for (let i = 12; i <= 24; i++) {
+  // console.log("the value of i is:" + i);
+}
+
+// #3 — Print the ODD numbers from 7 to 31
+
+for (let i = 7; i <= 31; i++) {
+  if (i % 2 != 0) {
+    // console.log(i);
+  }
+}
+
+// #4 — Print the EVEN numbers from 10 to -20
+
+for (let i = 10; i >= -20; i--) {
+  if (i % 2 == 0) {
+    // console.log(i);
+  }
+}
+
+/* #5 — Iterate through all numbers from 1 to 45. Print the following:
+
+For multiples of 3 print “Fizz”
+For multiples of 5 print “Buzz”
+For multiples of 3 and 5 print “FizzBuzz”
+
+*/
+
+for (let i = 1; i <= 45; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    //   console.log("FizzBuzz");
+    // } else if (i % 3 == 0) {
+    //   console.log("Fiz");
+    // } else if (i % 5 == 0) {
+    //   console.log("Buzz");
+  }
+}
+
+// #6 — Print all the elements of the following array
+
+let thisIsAnArray = ["element1", "element2", "element3", "element4"];
+
+for (let i = 0; i <= thisIsAnArray.length; i++) {
+  // console.log(thisIsAnArray[i]);
+}
+
+// #7 — Calculate the sum of all the numbers in the following array
+
+let numbersArray = [1, 13, 22, 123, 49];
+let sum = 0;
+
+for (let i = 0; i < numbersArray.length; i++) {
+  sum = sum + numbersArray[i];
+}
+
+console.log("The sum is: " + sum);
