a6e6031dbe3cb60cffdb3c2b4f2716d0fa659045
nsikakudoh <nsikak@sparknerds.io>
2023-02-24 16:15:57 -0700

    start local scope

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
-// Global scope vs local scope
-// any variable outside of the code block {} is said to be in global scope and can be accesssed anywhere in the program
-// gotchas: name collisions, modify by mistake
+// local scope
+// can not be accessed from outside code block
+// if - not VAR
 
 let name = "nsikak";
-name = "peter";
 
 function calculate() {
   // some other code...
-  console.log(name);
-  name = "orange";
-  function inner() {
-    name = "inner name value";
-    console.log(`this is from inner function ${name}`);
-  }
-  inner();
 }
-
 calculate();
 
 if (true) {
-  console.log(name);
-  name = "pants";
 }
 console.log(`my name is ${name} and i'm awesome`);
