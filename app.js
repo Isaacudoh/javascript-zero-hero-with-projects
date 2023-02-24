271c1416c819cedfa86588a718f652a75c9b92eb
nsikakudoh <nsikak@sparknerds.io>
2023-02-24 16:11:45 -0700

    local and global scope complete

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-// unary operators - typeof
-let text = "some text";
-console.log(typeof text); // operand
+// Global scope vs local scope
+// any variable outside of the code block {} is said to be in global scope and can be accesssed anywhere in the program
+// gotchas: name collisions, modify by mistake
 
-// binary operator - assignment
-let number = 3;
-let number2 = 2 + 5;
+let name = "nsikak";
+name = "peter";
 
-// ternary operator
-// condition ? (run if true) : (runs if false)
-
-let value = 1 < 0;
-
-value ? console.log("value is true") : console.log("value is false");
-
-if (value) {
-  console.log("value is true");
-} else {
-  console.log("value is false");
+function calculate() {
+  // some other code...
+  console.log(name);
+  name = "orange";
+  function inner() {
+    name = "inner name value";
+    console.log(`this is from inner function ${name}`);
+  }
+  inner();
 }
+
+calculate();
+
+if (true) {
+  console.log(name);
+  name = "pants";
+}
+console.log(`my name is ${name} and i'm awesome`);
