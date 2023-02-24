31c9f33bec16747bacf3520c7cdf3a2c37f18572
nsikakudoh <nsikak@sparknerds.io>
2023-02-24 09:19:36 -0700

    complete null and undefined

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
-// value vs reference
-// primitive data types
-// string, number, symbol, boolean, undefined, null,
-// Arrays, functions, objects = object
-// typeof
+// Null and undefined
+// both represent "no value"
 
-// when assigning primitive data type value to a variable any changes are made directly to that value, without affecting original value
-//  when assigning non=primitive data type value to a variable is done by reference as any changes will affect all the references.
+// undefined - "Javascript can not find value for this"
 
-let number = 1;
-let number2 = number;
-number2 = 7;
-console.log(`the first value is ${number}`);
-console.log(`the second value is ${number2}`);
+//  ## variable without value
+// ## missing function arguments
+// ## missing object properties
 
-let person = { name: "bob" };
-let person2 = person;
-person2.name = "susy";
-console.log(`the name of the first person is ${person.name}`);
-console.log(`the name of the second person is ${person2.name}`);
+// null - "developer sets the value"
+
+let number = 20 + null; // 20 + 0
+console.log(number);
+
+let number2 = 20 + undefined;
+console.log(number2);
