e3c51a4cb11b218da731c056e0dafca81575453a
nsikakudoh <nsikak@sparknerds.io>
2023-02-24 16:28:43 -0700

    complete local variable

--- app.js
+++ app.js
@@ -5,10 +5,14 @@
 let name = "nsikak";
 
 function calculate() {
-  // some other code...
+  let name = "john";
+  let age = 25;
+  console.log(age);
 }
+// console.log(age);
 calculate();
 
 if (true) {
+  let name = "john";
 }
 console.log(`my name is ${name} and i'm awesome`);
