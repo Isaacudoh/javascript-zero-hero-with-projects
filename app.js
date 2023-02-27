2b3b1d343734d0a76ad639f5ee6455cc95867669
nsikakudoh <nsikak@sparknerds.io>
2023-02-27 11:19:03 -0700

    code works

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-// Variable lookup
-// {} - code block
-
-let globalNumber = 5;
-
-function add(num1, num2) {
-  let globalNumber = 20;
-  let result = num1 + num2 + globalNumber;
-  function multiply() {
-    let globalNumber = 100;
-    let multiplyResult = result * globalNumber;
-    console.log(multiplyResult);
-  }
-  multiply();
-  return result;
+function greetMorning(name) {
+  let myName = "Nsikak";
+  console.log(`Goodmorning ${name}, my name is ${myName}`);
 }
 
-console.log(add(3, 4));
+function greetAfternoon(name) {
+  let myName = "Nsikak";
+  console.log(`Goodafternoon ${name}, my name is ${myName}`);
+}
+
+greetMorning("john");
+greetAfternoon("john");
+
+// callback functions, higher order functions, functions as first class objects/citizens
+
+// Functions are first class objects - stored in a variable (expression), pass as an argument to another function, return from the function (closure)
+
+// Higher order function - accepts another functon as an argument or returns another function as a result
+
+// callback function - passed to a another function as an argument and executed in that functoon
