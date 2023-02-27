14dda72ff410592bf817af86919b9b0b0c69ed15
nsikakudoh <nsikak@sparknerds.io>
2023-02-27 15:46:24 -0700

    start array methods

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-function morning(name) {
-  return `Goodmorning ${name.toUpperCase()}`;
+// Powerfull Arrar Methods
+// forEach, map, filter, find, reduce
+// Iterate over array - no for loop required
+//  Accept CALLBACK function as an argument, calls callback against each item in a array. Reference Item in the Callback Paramater.
+
+let numbers = [0, 1, 2, 3, 4];
+
+// show all numbers
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
 }
-function afternoon(name) {
-  return `Good afternoon ${name.repeat(3)}`;
-}
-function greet(name, callBack) {
-  let myName = "Nsikak";
-  console.log(`${callBack(name)}, my name is ${myName}`);
-}
-
-greet("john", morning);
-greet("bobo", morning);
-
-// callback functions, higher order functions, functions as first class objects/citizens
-
-// Functions are first class objects - stored in a variable (expression), pass as an argument to another function, return from the function (closure)
-
-// Higher order function - accepts another functon as an argument or returns another function as a result
-
-// callback function - passed to a another function as an argument and executed in that functoon
