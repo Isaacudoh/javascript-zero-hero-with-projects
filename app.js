7dba5b8b704f2a502427055c7ff8edadcc589798
nsikakudoh <nsikak@sparknerds.io>
2023-03-02 13:05:52 -0700

    complete find method

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-// filter
-// does return a new array
-// can manipulate the size of new array
-// returns based on condition
+// find
+// return an object
+// returns first match, if no match undefined
+// great for getting unique value
 
 let people = [
-  { name: "nsikak", age: 31, position: "developer" },
-  { name: "Peter", age: 25, position: "designer" },
-  { name: "Susy", age: 11, position: "boss" },
-  { name: "anna", age: 35, position: "the boss" },
+  { name: "nsikak", age: 31, position: "developer", id: 1 },
+  { name: "Peter", age: 25, position: "designer", id: 2 },
+  { name: "Susy", age: 11, position: "boss", id: 3 },
+  { name: "anna", age: 35, position: "the boss", id: 4 },
 ];
 
-let youngPeople = people.filter(function (person) {
-  return person.age <= 25;
+let names = ["bob", "peter", "susy"];
+console.log(
+  names.find(function (name) {
+    return name === "bob";
+  })
+);
+
+let person = people.find(function (person) {
+  return person.id === 3;
 });
 
-let developer = people.filter(function (person) {
-  return person.position === "developer";
+console.log(person.name);
+
+let person2 = people.filter(function (person) {
+  return person.id === 3;
 });
 
-console.log(developer);
+console.log(person2[0].name);
