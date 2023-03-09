d3bbd01eeb11944e37672e29178ffae9f24245e0
nsikakudoh <nsikak@sparknerds.io>
2023-03-09 15:15:04 -0700

    complete getElementByClassName

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 // select the element or group of elements that we want
 // decide the effect we want to apply to the selection
 
-// getElementsByTagName('tagname);
-//HTMLCollection = array-like object
-// index, length propery but not array methods (forEach)
+// getElementsByclassName('className');
 
-//querySelectorAll() in most cases
-// Nodelist - objects are collections of nodes
-// can run forEach
-// turn  them into array - spread operator [...]
-//after that can use any array properties
+//HTMLCollection
 
-let headings = document.getElementsByTagName("h2");
-headings[0].style.color = "red";
-console.log(headings);
-
-let items = document.getElementsByTagName("li");
-
-
-let betterItems = [...items]
-betterItems.forEach
+let listItems = document.getElementsByClassName("special");
+listItems[2].style.color = "blue";
+console.log(listItems);
