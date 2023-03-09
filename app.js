8bde77c6035a58aabaf14dd2f31a53d091ebdf71
nsikakudoh <nsikak@sparknerds.io>
2023-03-09 12:47:09 -0700

    get element by id

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
-//similar to css
-
-// select the element or group of elements that we want to affect
+// select the element or group of elements that we want
 // decide the effect we want to apply to the selection
 
-// many different ways
+// getElementById('element)
 
-document.body.style.backgroundColor = "purple";
-document.body.style.color = "yellow";
-document.getElementById("btn").style.color = "red";
-
-// assign to a variable
-let element = document.getElementById("element");
-
-// do something
-document.querySelector("element"); // do something
-
-//window object
-console.log(window);
-
-// returns a node object or a node list, which is an arraylike object
+let h1 = document.getElementById("title");
+h1.style.color = "red";
 
 let btn = document.getElementById("btn");
-let name = btn.nodeName;
-let css = btn.style;
+btn.style.color = "orange";
 
-console.log(btn);
-console.log(name);
+// document.getElementById("btn").style.backgroundColor = "blue";
+// document.getElementById("btn").style.backgroundColor = "white";
