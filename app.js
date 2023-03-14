38fbfacf267491e873b1f976719a371ee6bd3642
nsikakudoh <nsikak@sparknerds.io>
2023-03-14 07:33:15 -0600

    event overview

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-// css
-
-let random = document.querySelector(".random");
-
-// random.style.backgroundColor = "purple";
-// random.style.color = "white";
-// random.style.fontSize = "3rem";
-// random.style.textTransform = "capitalize";
-
-random.classList.add("title");
+let button = document.querySelector(".btn");
+button.style.background = "pink";
