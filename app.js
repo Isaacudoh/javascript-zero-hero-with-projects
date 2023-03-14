8b7c237eddf7acb8f7ba767287cec5c10c839d5a
nsikakudoh <nsikak@sparknerds.io>
2023-03-14 08:52:13 -0600

    finshed click event

--- app.js
+++ app.js
@@ -2,12 +2,27 @@
 // addEventListener()
 // what event, what to do
 
+// Example 1: annonymous function setup
 let btn = document.querySelector(".btn");
 let heading = document.querySelector("h2");
 
-function changeColors() {
-  console.log("hello");
-}
+btn.addEventListener("click", function () {
+  console.log("i have been clicked");
+  heading.classList.add("red");
+});
 
-btn.addEventListener("click", changeColors);
+// Example 2: function Reference setup
 
+// let btn = document.querySelector(".btn");
+// let heading = document.querySelector("h2");
+
+// function changeColors() {
+//   let hasClass = heading.classList.contains("red");
+//   if (hasClass) {
+//     heading.classList.remove("red");
+//   } else {
+//     heading.classList.add("red");
+//   }
+// }
+
+// btn.addEventListener("click", changeColors);
