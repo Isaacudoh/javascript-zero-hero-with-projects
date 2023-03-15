9cb072a3c599435b2f0e3544a30da6f644ef8f47
nsikakudoh <nsikak@sparknerds.io>
2023-03-14 19:39:23 -0600

    complete event/currentTarget

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// currentTarget
+// allows select dynamic elements
+// event propogation - order the events are fired
+// event bubbling - clicked element first then bubbles up --- default
+// event capturing - fires at the root and fires until reaches target
+
+let container = document.querySelector(".container");
+let list = document.querySelector(".list");
+
+function showBubbling(e) {
+  console.log("current target", e.currentTarget);
+  console.log("target", e.target);
+}
+
+list.addEventListener("click", showBubbling);
+container.addEventListener("click", showBubbling);
