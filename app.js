474f1fd372b6cab4c92c7ba585a79fe760f902e5
nsikakudoh <nsikak@sparknerds.io>
2023-03-14 19:55:46 -0600

    start event propogation example

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 // allows select dynamic elements
 // event propogation - order the events are fired
 // event bubbling - clicked element first then bubbles up --- default
-// event capturing - fires at the root and fires until reaches target
 
 let container = document.querySelector(".container");
-let list = document.querySelector(".list-items");
+let btn = document.querySelector(".btn");
+let heading = document.querySelector(".heading");
 
-function showBubbling(e) {
-  console.log("current target", e.currentTarget);
-  console.log("target", e.target);
-  if (e.target.classList.contains("link")) {
-    console.log("you clicked on a link");
-  }
+// heading.addEventListener("click", function () {
+//   console.log("hello");
+// });
+
+// another  way
+
+function sayHello() {
+  console.log("Hello there");
 }
 
-function stopPropagation(e) {
-  console.log("you clicked on a list");
-  e.stopPropagation();
-}
-
-list.addEventListener("click", showBubbling);
-container.addEventListener("click", showBubbling);
+heading.addEventListener("click", sayHello);
