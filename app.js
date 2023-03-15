c93e3cf716d21dbc6ea7e32ff120d77973db243a
nsikakudoh <nsikak@sparknerds.io>
2023-03-14 19:42:53 -0600

    bubbling

--- app.js
+++ app.js
@@ -4,12 +4,15 @@
 // event capturing - fires at the root and fires until reaches target
 
 let container = document.querySelector(".container");
-let list = document.querySelector(".list");
+let list = document.querySelector(".list-items");
 
 function showBubbling(e) {
   console.log("current target", e.currentTarget);
   console.log("target", e.target);
+  if (e.target.classList.contains("link")) {
+    console.log("you clicked on a link");
+  }
 }
 
 list.addEventListener("click", showBubbling);
-container.addEventListener("click", showBubbling);
+// container.addEventListener("click", showBubbling);
