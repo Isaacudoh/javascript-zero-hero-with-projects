3e4ab9be83ac4437cac6b038ab006bc357bc9b92
nsikakudoh <nsikak@sparknerds.io>
2023-03-16 11:10:02 -0600

    prevent default form behavior

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
-// allows select dynamic elements
-// event propogation - order the events are fired
-// event bubbling - clicked element first then bubbles up --- default
+// submit event listener
+// prevent default
+// how to get a value
 
-let container = document.querySelector(".container");
-let btn = document.querySelector(".btn");
-let heading = document.querySelector(".heading");
+let form = document.getElementById("form");
+let name = document.getElementById("name");
+let password = document.getElementById("password");
 
-// heading.addEventListener("click", function () {
-//   console.log("hello");
-// });
-
-// another  way
-
-function sayHello() {
-  console.log("Hello there");
-}
-
-// heading.addEventListener("click", sayHello);
-
-// to create h1 dynamically on button click
-btn.addEventListener("click", function () {
-  let element = document.createElement("h1"); // creates h1 & stores it in element var
-  element.classList.add("heading"); // add a class name of heading to each element
-  element.textContent = `I'm inside the container`; // the text in the heading
-  container.appendChild(element); // adds the new h1 to end of a node
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("form submitted");
 });
