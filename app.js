fb5e47ed64622146a694278409bd01a8fa9f4b3c
nsikakudoh <nsikak@sparknerds.io>
2023-03-17 10:56:53 -0600

    access image & width in console

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 /*
 
-The DOMContentLoaded event fires when the initial HTML document has bee completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading
+The load event fires when the whole page has been completely loaded, including all dependent resources such as stylesheets and images. This is in contrast to DOMContentLoaded, which is fired as soon as the page DOM has been loaded, without waiting for stylesheets, images, and subframes to finish loading.
 
-window.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('load', function(){
   // your code goes here
 })
 
-document.addEventListener....
-
 */
 
-let heading = document.querySelector("h1");
-
-window.addEventListener("DOMContentLoaded", function () {
-  console.log(heading);
-  heading.style.color = "blue";
+window.addEventListener("load", function () {
+  let image = document.querySelector("img");
+  console.log(image);
+  console.log(image.width);
 });
