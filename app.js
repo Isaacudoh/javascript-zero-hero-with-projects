33c71dd412fc415fe61dec1e2cfb2a5c547af84b
nsikakudoh <nsikak@sparknerds.io>
2023-03-17 10:28:47 -0600

    setIntervals & clearIntervals

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
 /*
-setTimeout - runs function once after specific time
+setInterval - runs function repeatedly, at specific intervals
 - pass function reference
 - duration in ms (1000 ms = 1 second)
 - default 0, will cover more extensively in async/await section
 - returns unique identifier
-- clearTimeout to cancel
-- on window object
+- clearInterval to cancel
 */
 
-function sayHello() {
-  console.log("hello john");
-}
-// setTimeout(sayHello, 1000);
-
-// alternative approach - ES6 arrow functions
-// setTimeout(function () {
-//   console.log("hello john");
-// }, 6000);
-
-// pass arguments
 function showScore(name, score) {
   console.log(`hello ${name}, your score is ${score}`);
 }
-// setTimeout(showScore,1000,'john',34)
 
+// setInterval(showScore, 2000);
+
+let firstID = setInterval(showScore, 1000, "bobo", 45);
+let secondID = setInterval(showScore, 4000, "susan", 45);
+
+clearInterval(firstID);
+clearInterval(secondID);
