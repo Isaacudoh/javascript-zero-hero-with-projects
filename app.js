5b8f9e2fa412664be7fe1284a9de098fb1807a62
nsikakudoh <nsikak@sparknerds.io>
2023-03-17 09:50:37 -0600

    local storagne with multiple values

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ localStorage.setItem("friends", JSON.stringify(friends));
 
 let values = JSON.parse(localStorage.getItem("friends"));
 console.log(values[2]);
+
+let fruits;
+if (localStorage.getItem("fruit")) {
+  fruits = JSON.parse(localStorage.getItem("fruits"));
+} else {
+  fruits = [];
+}
+
+console.log(fruits);
+// fruits.push("apple");
+fruits.push("orange");
+localStorage.setItem("fruits", JSON.stringify(fruits));
