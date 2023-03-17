d894002ce8b585ee179580df46f6fd6d5ba6066c
nsikakudoh <nsikak@sparknerds.io>
2023-03-17 11:42:32 -0600

    complete resize event

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
 /*
-
-The load event fires when the whole page has been completely loaded, including all dependent resources such as stylesheets and images. This is in contrast to DOMContentLoaded, which is fired as soon as the page DOM has been loaded, without waiting for stylesheets, images, and subframes to finish loading.
-
-window.addEventListener('load', function(){
-  // your code goes here
-})
-
+The resize event fires when the document view (window) has been resized
 */
 
-window.addEventListener("Load", function () {
-  let image = document.querySelector("img");
-  console.log(image);
-  console.log(image.width);
-});
-
-window.addEventListener("DOMContentLoaded", function () {
-  let image = document.querySelector("img");
-  console.log(image);
-  console.log(image.width);
+window.addEventListener("resize", function () {
+  console.log(window.innerWidth);
 });
